30eba7427736e9586ed725dfb693baad28bdc3cf
Wadie Mendja <mendja2014@gmail.com>
2022-02-18 13:19:31 +0100

    commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,11 @@
 const fileInput  = document.getElementById('formFileLg');
 const selectedImgDiv = document.getElementById('selectedImage');
+const animation = document.querySelector('.animation');
 
 // previewing image
 fileInput.addEventListener('change', (event)=> {
   const file = event.target.files[0];
   if (file)
     selectedImgDiv.innerHTML = `<img src="${URL.createObjectURL(file)}">`;
+    animation.innerHTML = "";
 });
