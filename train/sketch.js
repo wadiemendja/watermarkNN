06378e858f36e258d692668d71fd2c1bd8d43e38
Wadie Mendja <mendja2014@gmail.com>
2022-03-04 22:35:10 +0100

    commit

--- train/sketch.js
+++ train/sketch.js
@@ -1,37 +1,37 @@
-let wadieExample = [];
+let wadieExamples = [];
 let dahamExamples = [];
 let zeghamriExamples = [];
 let watermarkClassifier;
 
 function preload() {
    for (let i = 0; i < 10; i++) {
-      wadieExample[i] = loadImage(`../example_generator/sketch_220224a/data/wadiemendja${i}.png`);
+      wadieExamples[i] = loadImage(`../example_generator/sketch_220224a/data/wadiemendja${i}.png`);
       dahamExamples[i] = loadImage(`../example_generator/sketch_220224a/data/dahama${i}.png`);
       zeghamriExamples[i] = loadImage(`../example_generator/sketch_220224a/data/zeghamris${i}.png`);
    }
 }
 
-function setup() {}
+function setup() { }
 
-function startTraining() {
+function mousePressed() {
+   console.log("Training started !");
    watermarkClassifier = ml5.neuralNetwork({
       inputs: [64, 64, 4],
-      task: "imageClassification",
+      task: "Classification",
       debug: true
    });
-   for (let i = 0; i < wadieExample.length; i++) {
-      watermarkClassifier.addData({ image: wadieExample[i] }, { label: "Wadie Mendja" });
+   for (let i = 0; i < wadieExamples.length; i++) {
+      watermarkClassifier.addData({ image: wadieExamples[i] }, { label: "Wadie Mendja" });
       watermarkClassifier.addData({ image: dahamExamples[i] }, { label: "Daham A" });
       watermarkClassifier.addData({ image: zeghamriExamples[i] }, { label: "Zeghamri Salah" });
    }
    watermarkClassifier.normalizeData();
-   watermarkClassifier.train({ epochs: 50 }, () => {
+   watermarkClassifier.train({ epochs: 1 }, () => {
       // watermarkClassifier.save();
       console.log("Finished training");
    });
 }
 
-document.getElementById('train').addEventListener('click', startTraining);
 // loading dataset
 // const dataSet = document.getElementById('dataset');
 
